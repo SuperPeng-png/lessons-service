bde12e49e28369e5dfcc630a55089c3ab26cb74b
SuperPeng-png <denis.tchizhoff2017@yandex.ru>
2025-10-22 23:54:37 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/denis/tutor/tutor_site/TutorSiteApplication.java
@@ -0,0 +1,13 @@
+package com.denis.tutor.tutor_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denis/tutor/tutor_site/TutorSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denis.tutor.tutor_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
